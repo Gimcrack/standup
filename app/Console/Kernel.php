3161c98f75e8288d83fd27392faffe0f55a07473
Jeremy Bloomstrom <jeremy.bloomstrom@matsugov.us>
2017-09-18 08:57:54 -0800

    Add command to cache initial state

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\ScanAllClients;
+use App\Console\Commands\CacheInitialState;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-
+        CacheInitialState::class,
     ];
 
     /**
